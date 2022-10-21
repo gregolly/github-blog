c9aec0da4b19f1a0c2801355537a122ce1be932e
gregolly <gregollyff@hotmail.com>
2022-10-21 13:02:44 -0400

    Fixed the error of load the types on the request

--- src/pages/Home/components/Main/components/Content/components/CardIssue/components/InnerContent/index.tsx
+++ src/pages/Home/components/Main/components/Content/components/CardIssue/components/InnerContent/index.tsx
@@ -9,23 +9,21 @@ import {
 import { NavLink, useParams } from 'react-router-dom'
 import axios from 'axios'
 
-interface PropsContent {
-  content: {
-    html_url: string
-    title: string
-    author_association: string
-    created_at: Date
-    comments: string
-    body: string
-  }[]
+interface PropsInnerContent {
+  html_url: string
+  title: string
+  author_association: string
+  created_at: string
+  comments: string
+  body: string
 }
 
 export function InnerContent() {
   const { issuesid } = useParams()
-  const [content, setContent] = useState<PropsContent[]>([])
+  const [content, setContent] = useState<PropsInnerContent>()
 
   const getIssuesByID = useCallback(async () => {
-    const response = await axios.get(
+    const response = await axios.get<PropsInnerContent>(
       `https://api.github.com/repos/gregolly/github-blog/issues/${issuesid}`,
     )
 
@@ -43,18 +41,18 @@ export function InnerContent() {
       <HeaderContentInner>
         <InfoContent>
           <NavLink to="/">Voltar</NavLink>
-          <NavLink to={content.html_url}>Ver no github</NavLink>
-          <h2>{content.title}</h2>
+          <NavLink to={content?.html_url || ''}>Ver no github</NavLink>
+          <h2>{content?.title}</h2>
           <ul>
-            <li>{content.author_association}</li>
-            <li>{content.created_at}</li>
-            <li>{content.comments} comentários</li>
+            <li>{content?.author_association}</li>
+            <li>{content?.created_at}</li>
+            <li>{content?.comments} comentários</li>
           </ul>
         </InfoContent>
       </HeaderContentInner>
 
       <BodyContentContainer>
-        <p>{content.body}</p>
+        <p>{content?.body}</p>
       </BodyContentContainer>
     </ContainerInnerContent>
   )
